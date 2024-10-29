a207e6a7aa478e8aa52ee2bfcd4f05f22a729ea9
willsaurio <wmazeledonado8a@gmail.com>
2024-10-29 10:48:28 -0600

    Configuracion de la plantalla base y la creacion de nuevos archivos para el controlor y concatenacion de PHP en los archivos escritos

---
+++ views/plantilla.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title><?php echo COMPANY?></title>
+    <?php include "./views/inc/Link.php";?>
+</head>
+
+<body>
+
+    <!-- Main container -->
+    <main class="full-box main-container">
+        <!-- Nav lateral -->
+        <?php include "./views/inc/NavLateral.php"; ?>
+
+        <!-- Page content -->
+        <section class="full-box page-content">
+            <?php include "./views/inc/NavBar.php"; ?>
+
+        </section>
+
+    </main>
+    <?php include "./views/inc/Script.php"; ?>
+</body>
+
+</html>
