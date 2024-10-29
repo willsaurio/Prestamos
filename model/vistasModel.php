7e589d8047236205253f1c4d6e4a4fde586fb2d8
willsaurio <wmazeledonado8a@gmail.com>
2024-10-29 12:13:39 -0600

    Creacion de vista y eliminacion de algunos html que hacian espacion innecesario en el programa

--- model/vistasModel.php
+++ model/vistasModel.php
@@ -3,10 +3,11 @@ class vistaModel{
 
     /*-------Modelo para obtener vistas-------*/
     protected static function obtener_vistas_model($vistas){
-        $listablanca = [];
+        $listablanca = ["home", "client-list", "client-new","client-seach","client-update","company","home","item-list","item-new","item-search","item-update","reservation-list","reservation-new","reservation-pending","reservation-search",
+        "reservation-reservation","reservation-update","user-list","user-new","user-search","user-update"];
         if(in_array($vistas, $listablanca )){
-            if(is_file("./views/contenidos/".$vistas."-view.php")){
-                $contenido = "./views/contenidos/" . $vistas . "-view.php";
+            if(is_file("./views/contenidos/".$vistas."-views.php")){
+                $contenido = "./views/contenidos/" . $vistas . "-views.php";
             }else{
                 $contenido = "404";
             }
--- views/plantilla.php
+++ views/plantilla.php
@@ -9,7 +9,17 @@
 </head>
 
 <body>
+    <?php
+        $peticionAjax = false;
+        require_once "./controller/vistasControlador.php";
+        $IV = new vistasController();
 
+        $vistas = $IV->obtener_vista_controlador();
+
+        if($vistas=="login"|| $vistas =="404"){
+           require_once"./views/contenidos/".$vistas."-views.php";
+        }else{
+    ?>
     <!-- Main container -->
     <main class="full-box main-container">
         <!-- Nav lateral -->
@@ -17,12 +27,18 @@
 
         <!-- Page content -->
         <section class="full-box page-content">
-            <?php include "./views/inc/NavBar.php"; ?>
+            <?php 
+            include "./views/inc/NavBar.php"; 
+            
+            include $vistas;
+            ?>
 
         </section>
-
     </main>
-    <?php include "./views/inc/Script.php"; ?>
+    <?php
+        }
+     include "./views/inc/Script.php";
+    ?>
 </body>
 
 </html>
