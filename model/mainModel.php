b58fbf83cff79d14329b723536b11fb6299e6d3e
willsaurio <wmazeledonado8a@gmail.com>
2024-11-02 22:30:52 -0600

    configuracion de HASH y creaciones de numeros aleatorios

--- model/mainModel.php
+++ model/mainModel.php
@@ -1,25 +1,58 @@
 <?php
-    
-    if($peticionAjax){
-        require_once "../config/server.php";
-    }else{
-        require_once "./config/server.php";
+
+if ($peticionAjax) {
+    require_once "../config/server.php";
+} else {
+    require_once "./config/server.php";
+}
+
+class mainModel
+{
+
+    /*----- Funcion para conectar a la BD ----- */
+    protected static function conectar()
+    {
+        $conexion = new PDO(SGBD, USER, PASSWORD);
+        $conexion->exec("SET CHARACTER SET utf8");
+        return $conexion;
     }
 
-    class mainModel{
-      
-        /*----- Funcion para conectar a la BD ----- */
-        protected static function conectar(){
-            $conexion = new PDO(SGBD, USER, PASSWORD);
-            $conexion->exec("SET CHARACTER SET utf8");
-            return $conexion;
-        }
-
-        /*----- Funcion ejecutar consultas simples ----- */
-        protected static function ejecutar_consulta_simple($consulta){
-            $sql=self::conectar()->prepare($consulta);
-            $sql->execute();
-            return $sql;
-        }
-
+    /*----- Funcion ejecutar consultas simples ----- */
+    protected static function ejecutar_consulta_simple($consulta)
+    {
+        $sql = self::conectar()->prepare($consulta);
+        $sql->execute();
+        return $sql;
     }
+
+
+    /*----- Encriptador cadenas simples ----- */
+    public function encryption($string)
+    {
+        $output = FALSE;
+        $key = hash('sha256', SECRET_KEY);
+        $iv = substr(hash('sha256', SECRET_IV), 0, 16);
+        $output = openssl_encrypt($string, METHOD, $key, 0, $iv);
+        $output = base64_encode($output);
+        return $output;
+    }
+
+    /*----- Desencriptador cadenas simples ----- */
+    protected static function decryption($string)
+    {
+        $key = hash('sha256', SECRET_KEY);
+        $iv = substr(hash('sha256', SECRET_IV), 0, 16);
+        $output = openssl_decrypt(base64_decode($string), METHOD, $key, 0, $iv);
+        return $output;
+    }
+
+    /*----- Funcion generar codigos aleatorios ----- */
+    protected static function generar_codigo_aleatorio($letra,$longitud, $numero){
+        for($i=1; $i<=$longitud; $i++){
+            $aleatorio=rand(0,9);
+            $letra.=$aleatorio;
+        }
+        return $letra."-".$numero;
+    }
+
+}
