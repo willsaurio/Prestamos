a7f41cdbb7b53cbff3762faf5f4f39e369e28834
willsaurio <wmazeledonado8a@gmail.com>
2024-11-03 18:39:09 -0600

    Modelo para evitar algun tipo de inyecciones SQL en PHP y MVC

--- model/mainModel.php
+++ model/mainModel.php
@@ -55,4 +55,37 @@ class mainModel
         return $letra."-".$numero;
     }
 
+    /*----- Funcion generar codigos aleatorios ----- */
+    protected static function limpiar_cadena($cadena){
+        $cadena=trim($cadena);
+        $cadena=stripcslashes($cadena);
+        $cadena=str_ireplace("<script>", "",$cadena);
+        $cadena=str_ireplace("</script>", "",$cadena);
+        $cadena=str_ireplace("<script src", "",$cadena);
+        $cadena=str_ireplace("<script type =", "",$cadena);
+        $cadena=str_ireplace("SELECT * FROM", "",$cadena);
+        $cadena=str_ireplace("INSERT INTO", "",$cadena);
+        $cadena=str_ireplace("DELETE FROM", "",$cadena);
+        $cadena=str_ireplace("DROP TABLE", "",$cadena);
+        $cadena=str_ireplace("DROP DATABASE", "",$cadena);
+        $cadena=str_ireplace("TRUNCATE TABLE", "",$cadena);
+        $cadena=str_ireplace("SHOW TABLES", "",$cadena);
+        $cadena=str_ireplace("SHOW DATABASES", "",$cadena);
+        $cadena=str_ireplace("<?php ", "",$cadena);
+        $cadena=str_ireplace("?>", "",$cadena);
+        $cadena=str_ireplace("--", "",$cadena);
+        $cadena=str_ireplace(">", "",$cadena);
+        $cadena=str_ireplace("<", "",$cadena);
+        $cadena=str_ireplace("[", "",$cadena);
+        $cadena=str_ireplace("]", "",$cadena);
+        $cadena=str_ireplace("==", "",$cadena);
+        $cadena=str_ireplace("^", "",$cadena);
+        $cadena=str_ireplace("==", "",$cadena);
+        $cadena=str_ireplace(";", "",$cadena);
+        $cadena=str_ireplace("::", "",$cadena);
+        $cadena=stripcslashes($cadena);
+        $cadena=trim($cadena);
+        return $cadena;
+    }
+    
 }
