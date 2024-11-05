d2b8415c0d934aece32fb5c37654308091a61411
willsaurio <wmazeledonado8a@gmail.com>
2024-11-05 12:34:54 -0600

    Funcion con el envio de datos en AJAX

--- views/inc/Link.php
+++ views/inc/Link.php
@@ -13,11 +13,13 @@
 <!-- Sweet Alerts V8.13.0 CSS file -->
 <link rel="stylesheet" href="<?php echo SERVERURL; ?>views/css/sweetalert2.min.css">
 
-<!-- Sweet Alert V8.13.0 JS file-->
-<script src="./js/sweetalert2.min.js"></script>
+<!-- Sweet Alert V8.13.0 JS file -->
+<script src="<?php echo SERVERURL; ?>views/js/sweetalert2.min.js"></script>
 
 <!-- jQuery Custom Content Scroller V3.1.5 -->
 <link rel="stylesheet" href="<?php echo SERVERURL; ?>views/css/jquery.mCustomScrollbar.css">
 
 <!-- General Styles -->
 <link rel="stylesheet" href="<?php echo SERVERURL; ?>views/css/style.css">
+
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
--- views/inc/Script.php
+++ views/inc/Script.php
@@ -22,3 +22,5 @@
 </script>
 
 <script src="<?php echo SERVERURL; ?>views/js/main.js"></script>
+
+<script src="<?php echo SERVERURL; ?>views/js/alertas.js"></script>
